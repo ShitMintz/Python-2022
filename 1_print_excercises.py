9e378dbd682a51a225f389166b78bc2632865faf
ShitMintz <100096969+ShitMintz@users.noreply.github.com>
2022-02-22 12:13:50 +1300

    Did first excercise

---
+++ 1_print_excercises.py
@@ -0,0 +1,14 @@
+#print your own name
+print ("Regan de Boer")
+# Print your teachers name
+print ("Mr Smith")
+#Print the name of your school
+print ("Kings High School")
+#Print out your age
+print ("15")
+#print how many hours you have in your favourite game
+print ("1792 hours in GtaV")
+#print what year it will be in 25 years
+print (2022+25)
+#print out how old you will be in 100 years
+print (15+100)
---
+++ PrintExcercises.py
@@ -0,0 +1,10 @@
+#print your own name
+print ("Regan de Boer")
+# Print your teachers name
+print ("Mr Smith")
+#Print the name of your school
+print ("Kings High School")
+#Print out your age
+print ("15")
+#print how many hours you have in your favourite game
+print ("1792 hours in GtaV")
