41a69b110e171c06d7eb17f87ccfadeb21628d1c
ShitMintz <100096969+ShitMintz@users.noreply.github.com>
2022-02-23 13:22:39 +1300

    Create 2_print_excercises.py

---
+++ 2_print_excercises.py
@@ -0,0 +1,13 @@
+#1
+print ("Hey now, you're an all star\nGet your game on, go play\n Hey now, you're a rock star\n Get the show on, get paid \n(and all that glitter is gold)\n Only shooting stars break the mold.")
+#2
+print ("F**k off Hitler.")
+#3
+print ("This is a string")
+#4
+print ("an integer is a number")
+#5
+print ("A float is an integer with a decimal point")
+#6
+print (543/3)
+#7
