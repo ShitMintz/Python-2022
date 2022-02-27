2658e8dc824e7be381caed2adadcd4de62f732ce
ShitMintz <100096969+ShitMintz@users.noreply.github.com>
2022-02-28 09:52:25 +1300

    work today yes

--- 3_variables.py
+++ 3_variables.py
@@ -1,2 +1,42 @@
+#1
 player_lives = 3
-player
+#2
+player_name = "fred"
+#3
+player_money = 69
+#4
+player_phonenumber = 69696969696969696969
+#5
+name = "Regan"
+#6
+age = 15
+#7
+year = 2022
+#8
+print (name)
+#9
+print (age)
+#10
+print (year)
+#11
+surname = "de_Boer"
+#12
+school = "Kings_High"
+#13
+favourite_food = "dog"
+#14
+pet_name = "Sherman"
+#15
+chocolate_blocks = 69
+#16
+score = 21
+#17
+high_score = 69
+#18
+laps_completed = 5
+#19
+ammo = 420
+#20
+current_gear = "Good"
+#21
+current_speed = "mach 1"
---
+++ 4_modifyingVariables.py
@@ -0,0 +1,7 @@
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+print 
