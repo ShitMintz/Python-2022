4e51ae5bde68d12681bec2e8d84ee73c1b03fd12
ShitMintz <100096969+ShitMintz@users.noreply.github.com>
2022-03-23 14:57:28 +1300

    Update Game.py

---
+++ Game.py
@@ -0,0 +1,17 @@
+from adventurelib import *
+
+
+
+##########################
+#Define Rooms
+##########################
+space = Room("You are drifting in space and you see an abandoned ship flying around")
+cafeteria = Room("You are in a cafeteria wiht three exits")
+weapons = Room("you are in a weapons room with two exits")
+hallway1 = Room("you are in a hallway with four exits")
+o2 = Room("You are in an O2 room with one exit")
+navigation = Room("You are in a navigations room with one exit")
+shields = Room("You are in a shields room with two exits")
+hallway2 = Room("You are in a hallway with three exits")
+communications = Room("You are in a Communications room with one exit")
+storage = Room("You are in a storage room with three exits")
